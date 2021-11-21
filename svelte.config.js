c5d5ed20270257fb9cce7d9c986a427d77e7535d
Joshua Van Deren <livetoscout@gmail.com>
2021-11-20 18:42:12 -0800

    Updated project

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,19 @@
-import netlify from '@sveltejs/adapter-netlify'
-import preprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    adapter: netlify(),
-    target: '#svelte'
-  }
-}
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
 
-export default config
+export default config;
